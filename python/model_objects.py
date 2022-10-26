c536a30a679dcbb1ae740186f5d3258025fb2a4b
atifazad <atifazad83@gmail.com>
2022-10-26 17:02:25 +0200

    moving SupermarketCatalog class into model_objects.py
    removing unused catalog.py

--- python/model_objects.py
+++ python/model_objects.py
@@ -1,3 +1,16 @@
+class SupermarketCatalog:
+
+    def __init__(self):
+        self.products = {}
+        self.prices = {}
+
+    def add_product(self, product, price):
+        raise Exception("cannot be called from a unit test - it accesses the database")
+
+    def unit_price(self, product):
+        raise Exception("cannot be called from a unit test - it accesses the database")
+
+
 class Product:
     def __init__(self, name, unit):
         self.name = name
