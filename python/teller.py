b8504fb2f3e2efc35acded585ad1ab4f640532c9
atifazad <atifazad83@gmail.com>
2022-10-27 12:38:39 +0200

    moving a conditional check at correct place in Teller class

--- python/teller.py
+++ python/teller.py
@@ -26,30 +26,29 @@ class Teller:
     def handle_offers(self, the_cart, receipt):
         for product in the_cart._product_quantities.keys():
             quantity = the_cart._product_quantities[product]
-            if product in self.offers.keys():
-                discount = self.__compute_discount(product, quantity)
-
-                if discount:
-                    receipt.add_discount(discount)
+            discount = self.__compute_discount(product, quantity)
+            if discount:
+                receipt.add_discount(discount)
 
     def product_with_name(self, name):
         return self.catalog.products.get(name, None)
 
     def __compute_discount(self, product, quantity):
-        offer = self.offers[product]
-        quantity_as_int = int(quantity)
         discount = None
-        if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity_as_int > 2:
-            discount = self.__compute_discount_for_X_for_Y_offer(product, quantity, 3, 2)
+        if product in self.offers.keys():
+            offer = self.offers[product]
+            quantity_as_int = int(quantity)
+            if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity_as_int > 2:
+                discount = self.__compute_discount_for_X_for_Y_offer(product, quantity, 3, 2)
 
-        elif offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT and quantity_as_int >= 2:
-            discount = self.__compute_discount_for_X_for_amount_offer(product, quantity, 2)
+            elif offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT and quantity_as_int >= 2:
+                discount = self.__compute_discount_for_X_for_amount_offer(product, quantity, 2)
 
-        elif offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
-            discount = self.__compute_discount_for_X_for_amount_offer(product, quantity, 5)
-        
-        elif offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
-            discount = self.__compute_discount_for_x_percent_discount_offer(product, quantity)
+            elif offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
+                discount = self.__compute_discount_for_X_for_amount_offer(product, quantity, 5)
+            
+            elif offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
+                discount = self.__compute_discount_for_x_percent_discount_offer(product, quantity)
         
         return discount
 
